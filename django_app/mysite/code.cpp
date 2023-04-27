c3ed542ae9397812816050494547a8124061ecd8
sreechakramuttareddygari <muttareddygarisreechakra@gmail.com>
2023-04-27 09:20:31 +0530

    added 3rd problem and its testcases to database

--- django_app/mysite/code.cpp
+++ django_app/mysite/code.cpp
@@ -1,15 +1,60 @@
  #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    int count=0;
-    //cout<<n<<" ";
-    double sq_n = sqrt(n);
-    for(int i=1;i<=sq_n;i++){
-    if(i*i<=n){
-    count++;
-}
-}
-cout<<count;
-}
+
+int recurse(string& s, int i,vector<string>& ref,vector<string>& ret,int count){
+        if(i==s.size() || count>4)
+        {   ref.push_back(s.substr(i));
+            bool b=true;
+            for(int i=0;i<ref.size();i++){
+                if(ref[i].size()>3)
+                {b=false;
+                break;}
+                if(stoi(ref[i])<10 && ref[i].size()>1)
+                {b=false;
+                }
+                if(stoi(ref[i])<100 && ref[i].size()>2)
+                {b=false;
+                }
+                if(stoi(ref[i])>255)
+                {b=false;
+                //cout<<stoi(ref[i])<<" ";
+                }
+            }
+            if(b){
+                string a = ref[0];
+            for(int i=1;i<ref.size();i++){
+                a+='.'+ref[i];
+            }
+            ret.push_back(a);
+             //cout<<"\n";
+             }
+            ref.pop_back();
+            return 0;
+        }
+        for(int j=1;j<min((int)s.size()-i,4);j++){
+            ref.push_back(s.substr(i,j));
+            //cout<<s.substr(i,j)<<" "<<i<<" "<<j<<"\n";
+            recurse(s,i+j,ref,ret,count+1);
+            ref.pop_back();
+        }
+        return 0;
+
+    }
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ref;
+        vector<string> ret;
+        recurse(s,0,ref,ret,0);
+        return ret;
+    }
+    int main(){
+        string s;
+        cin >> s;
+        vector<string> ret = restoreIpAddresses(s);
+        if(ret.size()==0)
+        {cout<<"";
+      return 0;}
+        cout<<ret[0];
+        for(int i=1;i<ret.size();i++){
+          cout<<" "<<ret[i];
+        }
+    }
